004a9eb3ee1ab4b122c7581e94e6ee52bbf32e50
Hugocod <u.medioli@icloud.com>
2022-10-26 15:04:49 +0200

    created Product class

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Products {
+        ///////////////////////////////////////////////////////////////////////////////////////////////// GENERIC ATTRIBUTE
+        //Attributi Generici(necessari per ogni prodotto) 
+        public $price;
+        public $availability;
+        public $name;
+        public $description;
+        public $photo;
+        ///////////////////////////////////////////////////////////////////////////////////////////////// - / GENERIC ATTRIBUTE
+
+        function __construct( $_price, $_availability, $_name, $_description, $_photo )
+        {
+            $this -> price = $_price;
+            $this -> availability = $_availability;
+            $this -> name = $_name;
+            $this -> description = $_description;
+            $this -> photo = $_photo;
+        }
+    }
+
+?>
